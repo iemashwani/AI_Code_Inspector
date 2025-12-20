9aebb44aabf0730c1a8534b3922bd03cb1b6be80
AshwaniSingh12 <as3141538@gmail.com>
2025-12-20 17:24:44 +0530

    backend complete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,14 @@
 const express = require("express");
+const aiRoutes = require("./routes/ai.routes");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello from server");
 });
 
-app.get("/home", (req, res) => {
-  res.send("hello from home api");
-});
-
-app.get("/user", (req, res) => {
-  res.send("user created..");
-});
+app.use("/ai", aiRoutes);
 
 module.exports = app;
---
+++ Backend/src/controllers/ai.controller.js
@@ -0,0 +1,13 @@
+const aiService = require("../services/ai.service");
+
+module.exports.getResponse = async (req, res) => {
+  const code = req.body.code;
+
+  if (!code) {
+    return res.status(400).send("Code is required");
+  }
+
+  const response = await aiService(code);
+
+  res.send(response);
+};
